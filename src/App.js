638ca41c1328575678a9d9cb3cfe91800f0c3356
yxcho <choyangxuan@me.com>
2020-01-31 14:46:28 +0800

    node-sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomePage from "./homepage.component";
 
 function App() {
-  return (
-    <div>
-      Hello world
-    </div>
-  );
+  return <HomePage></HomePage>;
 }
 
 export default App;
